211c6899d285fc72f0b8bfaf2a3177e0c75f0b2c
Luis Eduardo Bérard <lebpmr@cesar.school>
2025-05-16 23:33:05 -0300

    aprimoramento do design

--- Main.c
+++ Main.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
+
+#define MAX_ESTRELAS 200
+#define MAX_NUVENS 10
+#define MAX_PASSAROS 15
 
 struct dinossauro {
     float x, y;
@@ -26,12 +31,32 @@ struct listaCactos {
 };
 typedef struct listaCactos ListaCactos;
 
+struct estrela {
+    Vector2 posicao;
+    float raio;
+    float brilho;
+    float velocidade;
+};
+typedef struct estrela Estrela;
+
+struct nuvem {
+    Vector2 posicao;
+    float escala;
+    float velocidade;
+    Color cor;
+};
+typedef struct nuvem Nuvem;
+
+struct passaro {
+    Vector2 posicao;
+    float velocidade;
+    float tamanho;
+    float tempoAnimacao;
+};
+typedef struct passaro Passaro;
+
 void inserir_cacto_final(ListaCactos *lista, float x, float y, float largura, float altura) {
     Cacto *novo = (Cacto*)malloc(sizeof(Cacto));
-    if (novo == NULL) {
-        printf("Erro ao alocar memória para novo cacto.\n");
-        exit(1);
-    }
     novo->x = x;
     novo->y = y;
     novo->largura = largura;
@@ -62,7 +87,12 @@ void atualizar_cactos(ListaCactos *lista, float velocidade) {
 void desenhar_cactos(ListaCactos *lista) {
     Cacto *atual = lista->inicio;
     while (atual != NULL) {
-        DrawRectangleRounded((Rectangle){atual->x, atual->y, atual->largura, atual->altura}, 0.2, 4, DARKGREEN);
+        Color verdeBase = (Color){34, 139, 34, 255};
+        Color verdeClaro = (Color){50, 205, 50, 255};
+        
+        DrawRectangleGradientV(atual->x, atual->y, atual->largura, atual->altura, verdeClaro, verdeBase);
+        DrawRectangleLinesEx((Rectangle){atual->x, atual->y, atual->largura, atual->altura}, 2, (Color){0, 100, 0, 255});
+        
         atual = atual->proximo;
     }
 }
@@ -82,14 +112,184 @@ int verificar_colisao(Dinossauro *dino, ListaCactos *lista) {
 }
 
 void desenhar_dinossauro(Dinossauro *dino) {
-    DrawRectangle(dino->x, dino->y, dino->largura, dino->altura, DARKGRAY);
-    DrawRectangle(dino->x + dino->largura - 10, dino->y - 15, 20, 20, DARKGRAY);
+    Color corBase = (Color){50, 50, 50, 255};
+    Color corDetalhe = (Color){70, 70, 70, 255};
+    
+    DrawRectangleGradientV(dino->x, dino->y, dino->largura, dino->altura, (Color){80, 80, 80, 255}, corBase);
+    
+    DrawRectangle(dino->x + dino->largura - 10, dino->y - 15, 20, 20, corDetalhe);
     DrawRectangle(dino->x + dino->largura + 5, dino->y - 10, 3, 3, BLACK);
-    DrawRectangle(dino->x + 5, dino->y + 10, 5, 3, DARKGRAY);
-    DrawRectangle(dino->x + 2, dino->y + dino->altura, 8, 5, DARKGRAY);
-    DrawRectangle(dino->x + dino->largura - 10, dino->y + dino->altura, 8, 5, DARKGRAY);
+    
+    DrawRectangle(dino->x + 5, dino->y + 10, 5, 3, corDetalhe);
+    DrawRectangle(dino->x + 2, dino->y + dino->altura, 8, 5, corDetalhe);
+    DrawRectangle(dino->x + dino->largura - 10, dino->y + dino->altura, 8, 5, corDetalhe);
+    
+    DrawRectangleLinesEx((Rectangle){dino->x, dino->y, dino->largura, dino->altura}, 1, (Color){30, 30, 30, 255});
 }
 
+void inicializar_estrelas(Estrela estrelas[], int largura, int altura) {
+    for (int i = 0; i < MAX_ESTRELAS; i++) {
+        estrelas[i].posicao = (Vector2){GetRandomValue(0, largura), GetRandomValue(0, altura/2)};
+        estrelas[i].raio = GetRandomValue(1, 3) / 2.0f;
+        estrelas[i].brilho = GetRandomValue(50, 100) / 100.0f;
+        estrelas[i].velocidade = GetRandomValue(1, 3) / 10.0f;
+    }
+}
+
+void atualizar_estrelas(Estrela estrelas[], int largura, int altura) {
+    for (int i = 0; i < MAX_ESTRELAS; i++) {
+        estrelas[i].brilho = 0.5f + 0.5f * sinf(GetTime() * estrelas[i].velocidade);
+        if (estrelas[i].brilho < 0.3f) estrelas[i].brilho = 0.3f;
+    }
+}
+
+void desenhar_estrelas(Estrela estrelas[]) {
+    for (int i = 0; i < MAX_ESTRELAS; i++) {
+        Color corEstrela = (Color){255, 255, 200, (unsigned char)(255 * estrelas[i].brilho)};
+        DrawCircleV(estrelas[i].posicao, estrelas[i].raio, corEstrela);
+        if (estrelas[i].raio > 1.0f) {
+            DrawCircleV(estrelas[i].posicao, estrelas[i].raio * 1.5f, Fade(corEstrela, 0.3f * estrelas[i].brilho));
+        }
+    }
+}
+
+void inicializar_nuvens(Nuvem nuvens[], int largura, int altura) {
+    for (int i = 0; i < MAX_NUVENS; i++) {
+        nuvens[i].posicao = (Vector2){GetRandomValue(-100, largura), GetRandomValue(30, altura/3)};
+        nuvens[i].escala = GetRandomValue(50, 120) / 100.0f;
+        nuvens[i].velocidade = GetRandomValue(5, 15) / 10.0f;
+        nuvens[i].cor = (Color){255, 255, 255, GetRandomValue(200, 240)};
+    }
+}
+
+void atualizar_nuvens(Nuvem nuvens[], int largura) {
+    for (int i = 0; i < MAX_NUVENS; i++) {
+        nuvens[i].posicao.x += nuvens[i].velocidade;
+        if (nuvens[i].posicao.x > largura + 100) {
+            nuvens[i].posicao.x = -100;
+            nuvens[i].posicao.y = GetRandomValue(30, 150);
+        }
+    }
+}
+
+void desenhar_nuvem(Vector2 posicao, float escala, Color cor) {
+    float raio = 20 * escala;
+    
+    DrawCircleV((Vector2){posicao.x + raio, posicao.y + raio}, raio, cor);
+    DrawCircleV((Vector2){posicao.x + raio * 2.5f, posicao.y + raio}, raio, cor);
+    DrawCircleV((Vector2){posicao.x + raio * 1.5f, posicao.y + raio * 0.7f}, raio * 0.8f, cor);
+    DrawCircleV((Vector2){posicao.x + raio * 1.5f, posicao.y + raio * 1.3f}, raio * 0.8f, cor);
+}
+
+void inicializar_passaros(Passaro passaros[], int largura, int altura) {
+    for (int i = 0; i < MAX_PASSAROS; i++) {
+        passaros[i].posicao = (Vector2){GetRandomValue(0, largura), GetRandomValue(50, altura/3)};
+        passaros[i].velocidade = GetRandomValue(100, 200) / 100.0f;
+        passaros[i].tamanho = GetRandomValue(5, 15) / 10.0f;
+        passaros[i].tempoAnimacao = GetRandomValue(0, 100) / 100.0f;
+    }
+}
+
+void atualizar_passaros(Passaro passaros[], int largura) {
+    for (int i = 0; i < MAX_PASSAROS; i++) {
+        passaros[i].posicao.x += passaros[i].velocidade;
+        passaros[i].tempoAnimacao += 0.1f;
+        
+        if (passaros[i].posicao.x > largura + 20) {
+            passaros[i].posicao.x = -20;
+            passaros[i].posicao.y = GetRandomValue(50, 150);
+        }
+    }
+}
+
+void desenhar_passaro(Vector2 posicao, float tamanho, float tempo) {
+    float asaOffset = sinf(tempo * 10) * 3 * tamanho;
+    Color corPassaro = (Color){139, 69, 19, 255};
+    
+    DrawTriangle(
+        (Vector2){posicao.x, posicao.y},
+        (Vector2){posicao.x + 10 * tamanho, posicao.y + asaOffset},
+        (Vector2){posicao.x + 10 * tamanho, posicao.y - asaOffset},
+        corPassaro
+    );
+}
+
+void desenhar_lua(Vector2 posicao, float raio) {
+    DrawCircleV(posicao, raio, (Color){220, 220, 220, 255});
+    DrawCircle(posicao.x - raio*0.3f, posicao.y - raio*0.2f, raio*0.15f, (Color){180, 180, 180, 255});
+    DrawCircle(posicao.x + raio*0.4f, posicao.y + raio*0.1f, raio*0.2f, (Color){180, 180, 180, 255});
+    DrawCircle(posicao.x - raio*0.1f, posicao.y + raio*0.3f, raio*0.1f, (Color){180, 180, 180, 255});
+    DrawCircleV(posicao, raio * 1.2f, Fade((Color){220, 220, 255, 255}, 0.1f));
+}
+
+void desenhar_montanhas(int largura, int altura) {
+    Color corMontanhas1 = (Color){50, 120, 80, 255};
+    Color corMontanhas2 = (Color){60, 150, 100, 255};
+    
+    DrawTriangle(
+        (Vector2){0, altura - 20},
+        (Vector2){largura * 0.3f, altura * 0.4f},
+        (Vector2){largura * 0.6f, altura - 20},
+        corMontanhas1
+    );
+    
+    DrawTriangle(
+        (Vector2){largura * 0.4f, altura - 20},
+        (Vector2){largura * 0.7f, altura * 0.5f},
+        (Vector2){largura, altura - 20},
+        corMontanhas2
+    );
+    
+    for (int i = 0; i < 20; i++) {
+        float x = GetRandomValue(0, largura);
+        float y = GetRandomValue(altura * 0.6f, altura - 30);
+        DrawCircle(x, y, 1, (Color){100, 150, 120, 255});
+    }
+}
+
+void desenhar_sol(Vector2 posicao, float raio, float tempo) {
+    Color corSol = (Color){255, 255, 100, 255};
+    Color corHalo = (Color){255, 200, 50, 100};
+    
+    DrawCircleV(posicao, raio, corSol);
+    
+    for (int i = 1; i <= 5; i++) {
+        float haloRaio = raio + i * 10 + sinf(tempo) * 5;
+        DrawCircleV(posicao, haloRaio, Fade(corHalo, 0.1f / i));
+    }
+    
+    int raios = 12;
+    for (int i = 0; i < raios; i++) {
+        float angle = 2 * PI * i / raios + tempo * 0.5f;
+        Vector2 start = {posicao.x + cosf(angle) * raio, posicao.y + sinf(angle) * raio};
+        Vector2 end = {posicao.x + cosf(angle) * (raio + 30 + sinf(tempo * 2 + i) * 10), 
+                   posicao.y + sinf(angle) * (raio + 30 + sinf(tempo * 2 + i) * 10)};
+        DrawLineEx(start, end, 3, Fade(corSol, 0.7f));
+    }
+}
+
+void desenhar_ceu_diurno(int largura, int altura, float tempo) {
+    Color topColor = (Color){50, 150, 255, 255};
+    Color bottomColor = (Color){135, 206, 235, 255};
+    
+    for (int y = 0; y < altura; y++) {
+        float ratio = (float)y / altura;
+        Color lineColor = (Color){
+            (unsigned char)(topColor.r + (bottomColor.r - topColor.r) * ratio),
+            (unsigned char)(topColor.g + (bottomColor.g - topColor.g) * ratio),
+            (unsigned char)(topColor.b + (bottomColor.b - topColor.b) * ratio),
+            255
+        };
+        DrawLine(0, y, largura, y, lineColor);
+    }
+    
+    for (int i = 0; i < 5; i++) {
+        float x = GetRandomValue(0, largura);
+        float y = GetRandomValue(0, altura/3);
+        float size = GetRandomValue(1, 3);
+        DrawCircle(x, y, size, (Color){255, 255, 255, 100});
+    }
+}
 
 int main(void) {
     float tempoJogo = 0.0f;
@@ -97,14 +297,24 @@ int main(void) {
     int larguraTela = 800;
     int alturaTela = 450;
     int pontuacao = 0;
+    float tempoGlobal = 0.0f;
 
-    InitWindow(larguraTela, alturaTela, "Jogo Dino");
+    InitWindow(larguraTela, alturaTela, "Jogo Dino - Paraíso Visual");
     InitAudioDevice();
     Sound somPulo = LoadSound("pulo.wav");
-    SetSoundVolume(somPulo, 0.6f); 
+    SetSoundVolume(somPulo, 0.6f);
     Sound somColisao = LoadSound("colisao.wav");
     SetTargetFPS(60);
-    srand(time(NULL)); 
+    srand(time(NULL));
+
+    Estrela estrelas[MAX_ESTRELAS];
+    inicializar_estrelas(estrelas, larguraTela, alturaTela);
+
+    Nuvem nuvens[MAX_NUVENS];
+    inicializar_nuvens(nuvens, larguraTela, alturaTela);
+
+    Passaro passaros[MAX_PASSAROS];
+    inicializar_passaros(passaros, larguraTela, alturaTela);
 
     Dinossauro *dino = (Dinossauro*)malloc(sizeof(Dinossauro));
     dino->x = 50;
@@ -123,55 +333,77 @@ int main(void) {
 
     int jogoAtivo = 1;
     int podeReiniciar = 0;
-    float distanciaUltimoCacto = 300.0f; 
+    float distanciaUltimoCacto = 300.0f;
     float distanciaMinima = 250.0f;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
 
+        tempoGlobal += GetFrameTime();
         tempoJogo += GetFrameTime();
         if (tempoJogo >= 20.0f) {
-            modoNoite = !modoNoite; 
+            modoNoite = !modoNoite;
             tempoJogo = 0.0f;
         }
 
         if (modoNoite) {
-            ClearBackground((Color){10, 10, 40, 255}); 
+            Color corTopo = (Color){5, 5, 15, 255};
+            Color corBase = (Color){20, 10, 40, 255};
+            for (int y = 0; y < alturaTela; y++) {
+                float proporcao = (float)y / alturaTela;
+                Color corLinha = (Color){
+                    (unsigned char)(corTopo.r + (corBase.r - corTopo.r) * proporcao),
+                    (unsigned char)(corTopo.g + (corBase.g - corTopo.g) * proporcao),
+                    (unsigned char)(corTopo.b + (corBase.b - corTopo.b) * proporcao),
+                    255
+                };
+                DrawLine(0, y, larguraTela, y, corLinha);
+            }
 
-            DrawCircle(larguraTela - 80, 80, 30, LIGHTGRAY);
-
-            DrawCircle(100, 50, 2, RAYWHITE);
-            DrawCircle(200, 70, 2, RAYWHITE);
-            DrawCircle(250, 40, 2, RAYWHITE);
-            DrawCircle(400, 90, 2, RAYWHITE);
-            DrawCircle(600, 60, 2, RAYWHITE);
-            DrawCircle(700, 30, 2, RAYWHITE);
-
-            DrawRectangle(0, alturaTela - 20, larguraTela, 20, (Color){50, 40, 20, 255});
+            atualizar_estrelas(estrelas, larguraTela, alturaTela);
+            desenhar_estrelas(estrelas);
+            desenhar_montanhas(larguraTela, alturaTela);
+            desenhar_lua((Vector2){larguraTela - 100, 100}, 40);
+            DrawRectangle(0, alturaTela - 20, larguraTela, 20, (Color){30, 25, 35, 255});
+            
+            for (int i = 0; i < 10; i++) {
+                float x = GetRandomValue(0, larguraTela);
+                DrawRectangle(x, alturaTela - 25, 3, 5, (Color){50, 45, 60, 255});
+            }
         } else {
-            DrawRectangleGradientV(0, 0, larguraTela, alturaTela, SKYBLUE, RAYWHITE);
-            DrawCircle(larguraTela - 80, 80, 40, YELLOW);
-            DrawRectangle(0, alturaTela - 20, larguraTela, 20, DARKBROWN);
-        }
-
-        if (!modoNoite) {
-            DrawCircle(150, 100, 20, WHITE);
-            DrawCircle(170, 90, 25, WHITE);
-            DrawCircle(190, 100, 20, WHITE);
-
-            DrawCircle(400, 70, 18, WHITE);
-            DrawCircle(420, 60, 22, WHITE);
-            DrawCircle(440, 70, 18, WHITE);
+            desenhar_ceu_diurno(larguraTela, alturaTela, tempoGlobal);
+            desenhar_sol((Vector2){larguraTela - 80, 80}, 40, tempoGlobal);
+            
+            atualizar_nuvens(nuvens, larguraTela);
+            for (int i = 0; i < MAX_NUVENS; i++) {
+                desenhar_nuvem(nuvens[i].posicao, nuvens[i].escala, nuvens[i].cor);
+            }
+            
+            atualizar_passaros(passaros, larguraTela);
+            for (int i = 0; i < MAX_PASSAROS; i++) {
+                desenhar_passaro(passaros[i].posicao, passaros[i].tamanho, passaros[i].tempoAnimacao + tempoGlobal);
+            }
+            
+            desenhar_montanhas(larguraTela, alturaTela);
+            
+            Color corChaoBase = (Color){139, 69, 19, 255};
+            Color corChaoTopo = (Color){160, 82, 45, 255};
+            DrawRectangleGradientV(0, alturaTela - 20, larguraTela, 20, corChaoTopo, corChaoBase);
         }
 
         for (int i = 0; i < larguraTela; i += 40) {
-            DrawRectangle(i, alturaTela - 25, 20, 2, GRAY);
+            Color corLinha = modoNoite ? (Color){80, 70, 90, 255} : (Color){101, 67, 33, 255};
+            DrawRectangle(i, alturaTela - 25, 20, 2, corLinha);
         }
 
         char textoPontuacao[20];
         sprintf(textoPontuacao, "Pontos: %d", pontuacao);
-        DrawText(textoPontuacao, 11, 11, 20, LIGHTGRAY);
-        DrawText(textoPontuacao, 10, 10, 20, DARKGRAY);
+        
+        Color corSombra = modoNoite ? (Color){100, 100, 150, 255} : (Color){50, 50, 50, 255};
+        Color corTexto = modoNoite ? (Color){200, 200, 255, 255} : (Color){240, 240, 100, 255};
+        
+        DrawText(textoPontuacao, 11, 11, 20, corSombra);
+        DrawText(textoPontuacao, 10, 10, 20, corTexto);
 
         if (jogoAtivo) {
             if (IsKeyPressed(KEY_SPACE) && !dino->pulando) {
@@ -219,16 +451,22 @@ int main(void) {
         desenhar_cactos(&cactos);
 
         if (!jogoAtivo) {
-            DrawText("GAME OVER!", (larguraTela - MeasureText("GAME OVER!", 40)) / 2 + 2, alturaTela / 2 - 40 + 2, 40, GRAY);
-            DrawText("GAME OVER!", (larguraTela - MeasureText("GAME OVER!", 40)) / 2, alturaTela / 2 - 40, 40, RED);
+            Color corSombraGO = modoNoite ? (Color){100, 0, 0, 255} : (Color){50, 50, 0, 255};
+            Color corGO = modoNoite ? (Color){255, 80, 80, 255} : (Color){255, 255, 0, 255};
+            
+            DrawText("GAME OVER!", (larguraTela - MeasureText("GAME OVER!", 40)) / 2 + 2, alturaTela / 2 - 40 + 2, 40, corSombraGO);
+            DrawText("GAME OVER!", (larguraTela - MeasureText("GAME OVER!", 40)) / 2, alturaTela / 2 - 40, 40, corGO);
 
             char textoFinal[30];
             sprintf(textoFinal, "Pontuação final: %d", pontuacao);
             int larguraTextoFinal = MeasureText(textoFinal, 20);
-            DrawText(textoFinal, (larguraTela - larguraTextoFinal) / 2, alturaTela / 2 + 0, 20, DARKGRAY);
+            
+            Color corFinal = modoNoite ? (Color){200, 200, 255, 255} : (Color){255, 255, 150, 255};
+            DrawText(textoFinal, (larguraTela - larguraTextoFinal) / 2, alturaTela / 2 + 0, 20, corFinal);
 
             int larguraTextoReinicio = MeasureText("Pressione ESPACO para reiniciar", 20);
-            DrawText("Pressione ESPACO para reiniciar", (larguraTela - larguraTextoReinicio) / 2, alturaTela / 2 + 30, 20, GRAY);
+            Color corReinicio = modoNoite ? (Color){180, 180, 220, 255} : (Color){255, 255, 180, 255};
+            DrawText("Pressione ESPACO para reiniciar", (larguraTela - larguraTextoReinicio) / 2, alturaTela / 2 + 30, 20, corReinicio);
 
             if (IsKeyReleased(KEY_SPACE)) podeReiniciar = 1;
 
